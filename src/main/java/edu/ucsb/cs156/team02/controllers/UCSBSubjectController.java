f0b0d0cbf0f4cde3a152acb1ca701cf6da9fcdf9
bobhe9606 <bobhe@umail.ucsb.edu>
2022-02-06 23:31:29 -0800

    get endpoint

--- src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
@@ -85,7 +85,7 @@ public class UCSBSubjectController extends ApiController{
         return savedUCSBSubject;
     }
 
- /*   @ApiOperation(value = "Get a UCSB Subject with given id")
+    @ApiOperation(value = "Get a UCSB Subject with given id")
     //@PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("")
     public ResponseEntity<String> getUCSBSubjectById(
@@ -102,7 +102,7 @@ public class UCSBSubjectController extends ApiController{
         String body = mapper.writeValueAsString(ucsbsub_error.sub);
         return ResponseEntity.ok().body(body);
     }
-*/
+/*
     @ApiOperation(value = "Update a single UCSBSubject")
     @PutMapping("")
     public ResponseEntity<String> putSubjectById(
@@ -121,7 +121,7 @@ public class UCSBSubjectController extends ApiController{
 
         String body = mapper.writeValueAsString(incomingUCSBSubject);
         return ResponseEntity.ok().body(body);
-    }
+    }*/
 
         public UCSBSubjectOrError doesUCSBSubjectExist(UCSBSubjectOrError ucsbsub_error) {
 

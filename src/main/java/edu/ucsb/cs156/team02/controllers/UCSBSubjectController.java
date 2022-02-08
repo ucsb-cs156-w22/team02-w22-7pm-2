bbee4b43bb1eae01cb3fd5003ee443b08e3cc7c7
bobhe9606 <81777445+bobhe9606@users.noreply.github.com>
2022-02-08 14:04:41 -0800

    DeleteMapping

--- src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;

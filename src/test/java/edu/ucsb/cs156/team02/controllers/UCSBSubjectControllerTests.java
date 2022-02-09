f0fcd11d111b6a1c53ec091bb6f7b500af731407
bobhe9606 <81777445+bobhe9606@users.noreply.github.com>
2022-02-08 16:48:08 -0800

    delete todo that don't exist

--- src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectControllerTests.java
@@ -247,6 +247,34 @@ public class UCSBSubjectControllerTests extends ControllerTestCase {
                 assertEquals("ucsb subject with id 15 deleted", responseString);
         }
 
+        // Test api /delete with Subject id that doesn't exist
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void api_subject__delete_subject_that_does_not_exist() throws Exception {
+                // arrange
+
+                        UCSBSubject subject1 = UCSBSubject.builder()
+                        .subjectTranslation("Test Subject Translation")
+                        .deptCode("Test Department Code")
+                        .collegeCode("Test College Code")
+                        .subjectCode("Test Subject Code")
+                        .relatedDeptCode("Test related department code")
+                        .inactive(true)
+                        .id(15L)
+                        .build();
+                        when(ucsbsubjectRepository.findById(eq(15L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(
+                        delete("/api/UCSBSubjects?id=15")
+                                .with(csrf()))
+                        .andExpect(status().isBadRequest()).andReturn();
+
+                // assert
+                verify(ucsbsubjectRepository, times(1)).findById(15L);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals("ucsb subject with id 15 not found", responseString);
+        }
 
 
 
